77deb1095bd73186664aae3dd91ac996de0d9310
Alejandro Figueredo <aflopez000430@gmail.com>
2024-12-29 11:30:42 -0500

    Fix6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "node:process";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno para el modo actual (desarrollo, producción, etc.)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    // Configuración de Vite
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV), // Define la variable global __APP_ENV__
+    },
+    // Puedes añadir otras configuraciones aquí si es necesario
+  };
+});
