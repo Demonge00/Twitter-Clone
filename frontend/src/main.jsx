50898841c3b283460365d16d5ce18fab23a25875
Alejandro Figueredo <aflopez000430@gmail.com>
2024-12-30 13:43:22 -0500

    Fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
